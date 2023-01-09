5c9d5d2439ab37181fb87c746709e0c829cb2c9e
Sean <sean@Seans-MBP.modem>
2023-01-09 15:21:36 +1100

    incorporated backend functionality

--- src/components/control/Control.tsx
+++ src/components/control/Control.tsx
@@ -1,8 +1,30 @@
-import React from 'react';
+import {useState, useEffect} from 'react';
 import ProgressRing from '../progressRing/ProgressRing';
 import style from './Control.module.scss';
+import { moveBy, getPosition, position, forward, backward, left, right } from '../../service/control';
 
 const Control: React.FunctionComponent = () => {
+    const [localPosition, setLocalPosition] = useState<position>({x: 0, y: 0, z: 0});
+    const [checkState, setCheckState] = useState(false);
+
+    useEffect(() => {
+        (async () => {
+            try {
+                const idPosition = await getPosition(2);
+                setLocalPosition({x: Math.round(idPosition.x), y: Math.round(idPosition.y), z: Math.round(idPosition.z)});
+                console.log(localPosition);
+            } catch (err) {
+                throw err;
+            };
+        })();
+    }, [checkState]);
+
+    const progress = (direction: position) => {
+        moveBy(direction, 2);
+        setCheckState(!checkState);
+    }
+
+    
     return (
         <div className={style.control}>
             <div className={style.control__stats}>
@@ -12,20 +34,20 @@ const Control: React.FunctionComponent = () => {
                 <h3 className={style.control__stats__cat}>Light Intensity <ProgressRing radius={20} stroke={2} progress={10} colour={"#FAAB36"}/></h3>
             </div>
             <div className={style.control__position}>
-                <h2>Position</h2>
-                <h3 className={style.control__position__coord}>X <span className={style.control__position__coord__dim}>0</span></h3>
-                <h3 className={style.control__position__coord}>Y <span className={style.control__position__coord__dim}>0</span></h3>
-                <h3 className={style.control__position__coord}>Z <span className={style.control__position__coord__dim}>0</span></h3>
+                <h2>Position</h2> 
+                <h3 className={style.control__position__coord}>X <span className={style.control__position__coord__dim}>{localPosition.x}</span></h3>
+                <h3 className={style.control__position__coord}>Y <span className={style.control__position__coord__dim}>{localPosition.y}</span></h3>
+                <h3 className={style.control__position__coord}>Z <span className={style.control__position__coord__dim}>{localPosition.z}</span></h3>
             </div>
 
             <div className={style.control__arrows}>
                 <section className={style.control__arrows__item}>
-                    <button>↑</button>
+                    <button onClick={() => {progress(forward)}}>↑</button>
                 </section>
                 <section className={style.control__arrows__item}>
-                    <button>←</button>
-                    <button>↓</button>
-                    <button>→</button>
+                    <button onClick={() => {progress(left)}}>←</button>
+                    <button onClick={() => {progress(backward)}}>↓</button>
+                    <button onClick={() => {progress(right)}}>→</button>
                 </section>
             </div>
         </div>
--- src/service/control.ts
+++ src/service/control.ts
@@ -4,16 +4,70 @@ export type position = {
     z: number;
 };
 
-export const moveBy = async (direction: position, id: number) => {
-    const positionChange = await fetch(`localhost:8080/position/${id}`, {
-        method: 'POST',
-        body: JSON.stringify({
-            x: direction.x,
-            y: direction.y,
-            z: direction.z,
-        }),
-    });
-    const response: Promise<position> = await positionChange.json();
+export const backward: position = {
+    x: 0,
+    z: 0,
+    y: 1,
+};
+export const forward: position = {
+    x: 0,
+    y: -1,
+    z: 0,
+};
+export const left: position = {
+    x: -1,
+    y: 0,
+    z: 0,
+};
+export const right: position = {
+    x: 1,
+    y: 0,
+    z: 0,
+};
 
-    return response;
+export const moveBy = async (direction: position, id: number) => {
+    try {
+        console.log('attempting to move');
+        const positionChange = await fetch(
+            `http://localhost:8080/position/${id}`,
+            {
+                method: 'POST',
+                // mode: 'no-cors',
+                headers: {
+                    Accept: 'application/json',
+                    'Content-Type': 'application/json',
+                    'Access-Control-Allow-Origin': '*',
+                },
+                body: JSON.stringify(direction),
+            }
+        );
+        console.log('body sent');
+        const response: Promise<position> = await positionChange.json();
+        console.log('moved');
+        return await response;
+    } catch (err) {
+        throw err;
+    }
+};
+
+export const getPosition = async (id: number) => {
+    try {
+        console.log('Retrieving position location');
+        const thisPosition = await fetch(
+            `http://localhost:8080/position/${id}`,
+            {
+                method: 'GET',
+                // mode: 'no-cors',
+                headers: {
+                    Accept: 'application/json',
+                    'Content-Type': 'application/json',
+                    'Access-Control-Allow-Origin': '*',
+                },
+            }
+        );
+        const response = await thisPosition.json();
+        return response;
+    } catch (err) {
+        throw err;
+    }
 };
